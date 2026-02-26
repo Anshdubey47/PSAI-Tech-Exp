a1ca5c5cfdaa0cff240673e3a40aea0a6257c6a4
Anshdubey47 <135658059+Anshdubey47@users.noreply.github.com>
2026-02-26 15:54:57 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
   return (
     <Layout>
       <HeroSection />
-      <OfficialDistributorSection />
+      <ODSection/>
       <ServicesSection />
       <ClientsSection />
       <WhyChooseUsSection />
